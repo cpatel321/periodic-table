efa27c1dd4354aa694ac096e92760699082f3d04
Chandrabhan <chandrabhanpatel321@gmail.com>
2023-06-10 02:52:00 +0530

    Desktop ready, let's test

--- feedback.php
+++ feedback.php
@@ -2,14 +2,14 @@
 
 // $conn = mysqli_connect("fdb18.biz.nf" ,  "3399456_newdb" , "l5iRPu8vDuWLo" , "3399456_newdb" );
 
-$conn = mysqli_connect("localhost" ,  "root" , "" , "feedback" );
+$conn = mysqli_connect("fdb18.biz.nf" ,  "3399456_newdb" , "l5iRPu8vDuWLo" , "3399456_newdb" );
 
 
 if($conn){
-?><script>alert("connected to the Database.");</script><?php
+
 }
 else{
-?><script>alert(" not connected.Server is not responding. Try again later.");</script><?php
+?><script>alert("Error connecting to database, Please try again later.");</script><?php
 }
 
 if(!empty($_POST["submit"])) {
@@ -21,7 +21,7 @@ $query = "INSERT INTO feedback (name, email, message) VALUES ('" . $name. "', '"
 mysqli_query($conn, $query);
 	$insert_id = mysqli_insert_id($conn);
 if(!empty($insert_id)) {
-	 ?><script>  alert( "Your contact information is saved successfully.You may send feedbacks as many times you want.");</script>
+	 ?><script>  alert( "Thanks for your Feedback");</script>
 	  <?php
 	}
 }
